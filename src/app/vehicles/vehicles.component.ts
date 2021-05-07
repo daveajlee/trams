74f5d536fdb4caffc949174104164469ec636b84
David A J Lee <daveajlee@users.noreply.github.com>
2021-05-07 22:14:30 +0200

    Add missing pictures and add ability to search by fleet number

--- src/app/vehicles/vehicles.component.ts
+++ src/app/vehicles/vehicles.component.ts
@@ -3,6 +3,7 @@ import {Subscription} from 'rxjs';
 import {Vehicle} from './vehicle.model';
 import {VehiclesService} from './vehicles.service';
 import {DataService} from '../shared/data.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-vehicles',
@@ -17,13 +18,15 @@ export class VehiclesComponent implements OnInit, OnDestroy {
 
   vehicles: Vehicle[];
   subscription: Subscription;
+  searchSubscription: Subscription;
 
   /**
    * Create a new vehicles component which constructs a data service and a vehicle service to retrieve data from the server.
+   * @param http an http client which can make http calls
    * @param dataService which contains the HTTP connection to the server
    * @param vehiclesService which formats the HTTP calls into a way which the frontend can read and render.
    */
-  constructor(private dataService: DataService, private vehiclesService: VehiclesService ) { }
+  constructor(private http: HttpClient, private dataService: DataService, private vehiclesService: VehiclesService ) { }
 
   /**
    * Initialise a new stops component which maintains a list of stops that can be updated and set from the server calls.
@@ -34,11 +37,20 @@ export class VehiclesComponent implements OnInit, OnDestroy {
     });
   }
 
+  searchByFleetNumber(searchValue: string): void {
+    console.log('Search was: ' + searchValue);
+    this.searchSubscription = this.http.get<Vehicle[]>('http://localhost:8080/trams-operations/' +
+        'vehiclesCompanyFleetNumber?fleetNumber=' + searchValue).subscribe(vehicleInfos => {
+      this.vehicles = vehicleInfos;
+    });
+  }
+
   /**
    * Destroy the subscription when the component is destroyed.
    */
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+    this.searchSubscription.unsubscribe();
   }
 
 }
