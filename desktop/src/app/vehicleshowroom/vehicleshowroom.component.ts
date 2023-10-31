123b7f3b16f17f40310b34c267f7de2eb964f248
daveajlee <daveajlee@users.noreply.github.com>
2023-10-31 21:14:18 +0100

    Switch between more vehicle types in desktop client

--- desktop/src/app/vehicleshowroom/vehicleshowroom.component.ts
+++ desktop/src/app/vehicleshowroom/vehicleshowroom.component.ts
@@ -3,6 +3,7 @@ import {VehicleModel} from "../vehicles/vehiclemodel.model";
 import {GameService} from "../shared/game.service";
 import {Router} from "@angular/router";
 import {DatePipe} from "@angular/common";
+import {Vehicle} from "../vehicles/vehicle.model";
 
 @Component({
   selector: 'app-vehicleshowroom',
@@ -25,7 +26,10 @@ export class VehicleshowroomComponent {
    */
   constructor(private gameService2: GameService, public router: Router, private datePipe: DatePipe) {
     this.gameService = gameService2;
-    this.models = [new VehicleModel('MyBus Single Decker', 44, 36, 85000.0, 'assets/Bus.jpg')];
+    this.models = [new VehicleModel('MyBus Single Decker', 'Single Decker Bus', 44, 36, 85000.0, 'assets/singledecker-bus-pixabay.png'),
+        new VehicleModel('MyBus Double Decker', 'Double Decker Bus', 78, 25,160000, 'assets/doubledecker-bus-pixabay.png' ),
+        new VehicleModel('MyBus Bendy', 'Bendy Bus', 48, 97, 190000, 'assets/bendybus-albertstoynov-unsplash.jpg'),
+        new VehicleModel('MyTram Tram 1', 'Tram',104, 83, 280000, 'assets/tram-pixabay.png')];
     this.currentDisplay = 0;
     this.quantity = 1;
     this.currentDate = new Date();
@@ -65,7 +69,23 @@ export class VehicleshowroomComponent {
   }
 
   onPurchaseVehicle(): void {
-    alert('Coming Soon!');
+    // First we determine the next fleet number,
+    var vehicles = this.gameService.getGame().vehicles;
+    var highestFleetNumberSoFar = 0;
+    for ( var i = 0; i < vehicles.length; i++ ) {
+      if ( parseInt(vehicles[i].fleetNumber) > highestFleetNumberSoFar ) {
+        highestFleetNumberSoFar = parseInt(vehicles[i].fleetNumber);
+      }
+    }
+    const additionalProps = new Map<string, string>();
+    additionalProps.set('Model', this.getVehicleType());
+    additionalProps.set('Age', '0 months');
+    additionalProps.set('Standing Capacity', '' + this.getVehicleStandingCapacity());
+    additionalProps.set('Seating Capacity', '' + this.getVehicleSeatingCapacity());
+    additionalProps.set('Value', '' + this.getVehiclePurchasePrice());
+    this.gameService.getGame().addVehicle(new Vehicle('' + (highestFleetNumberSoFar+1), this.models[this.currentDisplay].modelType, '',
+        '', '', 0, additionalProps));
+    this.router.navigate(['management']);
   }
 
   backToManagementScreen(): void {
@@ -78,6 +98,13 @@ export class VehicleshowroomComponent {
 
   checkDisableNext():boolean {
     return (this.currentDisplay + 1) >= this.models.length;
+  }
+
+  moveToPreviousVehicleType(): void {
+    this.currentDisplay -= 1;
+  }
+  moveToNextVehicleType(): void {
+    this.currentDisplay += 1;
   }
 
 }
