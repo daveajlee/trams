829731738907d04a389292beb718b4a00df789c9
David A J Lee <dave@davelee.de>
2024-03-31 22:17:15 +0200

    Simulate time and passenger satisfaction

--- desktop/src/app/livesituation/livesituation.component.ts
+++ desktop/src/app/livesituation/livesituation.component.ts
@@ -3,6 +3,7 @@ import {Router} from "@angular/router";
 import {GameService} from "../shared/game.service";
 import {Route} from "../routes/route.model";
 import {ScheduleModel} from "../stops/stop-detail/schedule.model";
+import {TimeHelper} from "../shared/time.helper";
 
 @Component({
   selector: 'app-livesituation',
@@ -14,6 +15,7 @@ export class LivesituationComponent implements OnInit {
   gameService: GameService;
   selectedRoute: String;
   simulationRunning: boolean;
+  interval: any;
 
   constructor(private gameService2: GameService, public router: Router) {
     this.gameService = gameService2;
@@ -57,8 +59,9 @@ export class LivesituationComponent implements OnInit {
   }
 
   getCurrentPosition(schedule: ScheduleModel): string {
+    console.log('We are at schedule ' + schedule.scheduleId);
     var currentDateTime = this.gameService.getGame().currentDateTime;
-    var currentTime = currentDateTime.getHours() + ":" + currentDateTime.getMinutes();
+    var currentTime = TimeHelper.formatTimeAsString(currentDateTime);
     for ( let i = 0; i < schedule.services.length; i++ ) {
       for ( let j = 0; j < schedule.services[i].stopList.length; j++ ) {
         // If we exactly match the departure time then this is where we are.
@@ -68,17 +71,20 @@ export class LivesituationComponent implements OnInit {
         // If we are before departure time but after arrival time then we are also here.
         else if ( schedule.services[i].stopList[j].departureTime > currentTime
             && schedule.services[i].stopList[j].arrivalTime <= currentTime) {
+          console.log(schedule.services[i].stopList[j].departureTime + ' with ' + currentTime);
           return schedule.services[i].stopList[j].stop;
         }
         // If we are before both departure time and arrival time then we are at the previous stop if there was one.
         else if ( schedule.services[i].stopList[j].departureTime > currentTime
             && schedule.services[i].stopList[j].arrivalTime > currentTime
+            && schedule.services[0].stopList[0].arrivalTime < currentTime // Ensure service has started
             && j != 0 ) {
           return schedule.services[i].stopList[j-1].stop;
         }
         // If there was not a previous stop then it is the previous service last stop where we are at if there was one.
         else if ( schedule.services[i].stopList[j].departureTime > currentTime
             && schedule.services[i].stopList[j].arrivalTime > currentTime
+            && schedule.services[0].stopList[0].arrivalTime < currentTime // Ensure service has started
             && j === 0
             && i != 0 ) {
           return schedule.services[i-1].stopList[schedule.services[i-1].stopList.length-1].stop;
@@ -96,6 +102,25 @@ export class LivesituationComponent implements OnInit {
 
   setSimulationRunning(value: boolean) {
     this.simulationRunning = value;
+    if ( value === true ) {
+      this.interval = setInterval(() => {
+        // Increase the time by 15 minutes,
+        this.gameService.getGame().currentDateTime = new Date(this.gameService.getGame().currentDateTime.getTime() + 100*60000);
+        // Decrease or increase the passenger satisfaction by a maximum of 2 in either plus or minus direction,
+        var randomDiff = Math.random() * (4);
+        this.gameService.getGame().passengerSatisfaction = Math.round(this.gameService.getGame().passengerSatisfaction + (randomDiff-2));
+        // Ensure that passenger satisfaction is between 0 and 100.
+        if ( this.gameService.getGame().passengerSatisfaction < 0 ) {
+          this.gameService.getGame().passengerSatisfaction = 0;
+        } else if ( this.gameService.getGame().passengerSatisfaction > 100 ) {
+          this.gameService.getGame().passengerSatisfaction = 100;
+        }
+        }, 10000);
+    } else {
+      if (this.interval) {
+        clearInterval(this.interval);
+      }
+    }
   }
 
   backToManagementScreen(): void {
