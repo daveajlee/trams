60f9ef68898472798db0857fceda44ce2aef0c8b
David A J Lee <dave@davelee.de>
2025-09-24 20:21:33 +0200

    API should be documented in the server instead of the website.

--- docs/src/pages/Api.jsx
+++ docs/src/pages/Api.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import SwaggerUI from "swagger-ui-react"
-import "swagger-ui-react/swagger-ui.css"
 import {Card, Col, Container, Row} from "react-bootstrap";
 import apiImage from "../douglas-lopes-ehyV_XOZ4iA-unsplash.jpg";
 
@@ -11,16 +9,12 @@ const Api = () => (
                 <Card>
                     <Card.Img variant="top" className="img-responsive w-25 rounded mx-auto d-block mt-3" src={apiImage} />
                     <Card.Title className="mt-5 text-center">API</Card.Title>
-                    <Card.Body className="text-center mb-5">The current API of TraMS is displayed below. Please
-                    note that this API should only be used as reference documentation. The functionality to
-                    try out the API is not available on this page. If you would like to test the API or test a
+                    <Card.Body className="text-center mb-5">The current API of TraMS can be accessed
+                        by running the TraMS server and then the following URL: <br/><br/><span style={{fontWeight: "bold"}}>http://server:port/swagger-ui/index.html</span> <br/><br/> If you would like to test the API or test a
                     client you have built for the API then you need to download TraMS Server from the Download
                     page.</Card.Body>
                 </Card>
             </Col>
-        </Row>
-        <Row>
-            <SwaggerUI url="swagger.json" />
         </Row>
     </Container>
 );
